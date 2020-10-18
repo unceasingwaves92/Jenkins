b3902c1f0821fab28e6aa3f4bc3ef1ad5509557c
TestLeaf <homeworks@testleaf.com>
2020-10-18 10:57:41 +0530

    Code changes - 2

--- ScheduleJob/src/job/PrintConsole.java
+++ ScheduleJob/src/job/PrintConsole.java
@@ -4,7 +4,7 @@ public class PrintConsole {
 
 	public static void main(String[] args) {
 		
-		System.out.println("Hurray, the first Jenkins Job");
+		System.out.println("Hurray, the second Jenkins Job");
 
 	}
 
