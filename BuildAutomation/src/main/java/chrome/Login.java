4d8ae630df50698eef89c287bd4f013ad319c0ca
Babu <homeworks@testleaf.com>
2020-10-18 13:01:46 +0530

    This is baseline automation code for Jenkins to trigger test from Maven build

---
+++ BuildAutomation/src/main/java/chrome/Login.java
@@ -0,0 +1,23 @@
+package chrome;
+
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class Login {
+	
+	@Test
+	public void loginToLeafTaps() {
+		
+		WebDriverManager.chromedriver().setup();
+		ChromeDriver driver = new ChromeDriver();
+		driver.get("http://leaftaps.com/opentaps");
+		driver.findElementById("username").sendKeys("DemoSalesManager");
+		driver.findElementById("password").sendKeys("crmsfa");
+		driver.findElementByClassName("decorativeSubmit").click();
+		driver.findElementByClassName("decorativeSubmit").click();
+		driver.close();
+	}
+
+}
